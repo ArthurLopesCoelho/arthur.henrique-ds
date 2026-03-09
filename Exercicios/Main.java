c7f4beb60757cbf380109366a0a206418ae8bc29
Arthur Henrique <arthurcoelho6235@gmail.com>
2026-03-09 09:11:00 -0300

    Arquivos Aula 1

---
+++ Exercicios/Main.java
@@ -0,0 +1,16 @@
+    import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero");
+        int numero = scanner.nextInt();
+        if (numero%2==0)
+        {System.out.println("O numero "+numero+" e par");}
+        else
+        {System.out.println("O numero "+numero+" e impar");}
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # arthur.henrique-ds
 Repositório de aulas de desenvolvimento de sistemas
+
+Estudar básico de Java
+Baixar NetBeans
