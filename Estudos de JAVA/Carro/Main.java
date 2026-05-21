45deb9c73076e6cd180961dad277a430a1521896
Arthur Henrique <arthurcoelho6235@gmail.com>
2026-05-21 12:04:29 -0300

    Trabalho Classes e Objetos

---
+++ Estudos de JAVA/Carro/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Moto moto1 = new Moto();
+        moto1.marca = "Yamaha";
+        moto1.modelo = "MT-03";
+        moto1.ano = 2024;
+        moto1.cor = "Preta";
+        moto1.cilindradas = 321;
+        moto1.preco = 25000.0;
+    }
+}
---
+++ Estudos de JAVA/Carro/Moto.java
@@ -0,0 +1,8 @@
+public class Moto{
+    String marca;
+    String modelo;
+    int ano;    
+    String cor;
+    int cilindradas;
+    double preco;
+}
